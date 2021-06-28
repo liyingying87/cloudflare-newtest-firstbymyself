255703e5bd9f25f8ca7a64ef0d5e92712fc97524
liyingying87 <1418845580@qq.com>
2021-06-28 19:38:21 +0800

    跟着做main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import router from './router'
 import Vuesax from 'vuesax'
 import axios from 'axios';
 import Vuex from 'vuex'     //状态管理工具
-// import store from './vuex/store'  //引入vuex的状态仓库
-// import NProgress from 'nprogress'    //页面顶部加载条和样式
+import store from './vuex/store'  //引入vuex的状态仓库
+import NProgress from 'nprogress'    //页面顶部加载条和样式
 import 'vuesax/dist/vuesax.css'
 import 'nprogress/nprogress.css'
 import 'lib-flexible/flexible'
 //lib-flexible插件,移动端适配解决方案。https://github.com/amfe/article/issues/17
-// import directive from "./assets/util/directive";
+import directive from "./assets/util/directive";
 import VueAxios from 'vue-axios';
 //兼容ie
 import "babel-polyfill";
@@ -20,9 +20,18 @@ const commonUtil = require('./assets/util/common')
 const baseConfig = require('./assets/util/baseConfig')
 import constantsMap from './assets/util/constant'   //项目内使用到的常量
 import vueUtil from './assets/util/vue-util'   //项目内使用到的常量
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+let baseURL = ''
+if (process.env.NODE_ENV === 'development') {      //这里配置项目开发和上线后的baseURL
+  baseURL = '/api'
+} else {
+  baseURL = 'http://203.195.156.57:3000/api'
+}
+Vue.use(directive)
 Vue.use(VueAxios,axios);
-
-
+Vue.use(mavonEditor)
 Vue.use(Vuex)
 Vue.config.productionTip = false
 
