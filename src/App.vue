9a59b4b9eb873f9b630da74a903d8cefb01d2e57
liyingying87 <1418845580@qq.com>
2021-11-02 19:04:46 +0800

    初步试一试

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+<!--    <router-view/>-->
+    <Test_MapTrack></Test_MapTrack>
   </div>
+
 </template>
 
 <script>
+import Test_MapTrack from "@/components/Test_MapTrack";
 export default {
   name: 'App',
   mounted(){
 
   },
   components: {
-
+    Test_MapTrack,
   },
   data(){
     return{
